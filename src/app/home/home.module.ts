6cbf97cb1920fad0c95fe98b0e22cedf2a333770
Soumaya.Mejri <Soumaya.Mejri@yesss.tn>
2020-04-19 13:54:45 +0100

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { HomePage } from './home.page';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild([
@@ -20,4 +22,9 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+
+export class HomePageModule { 
+
+  constructor() { }
+
+}
